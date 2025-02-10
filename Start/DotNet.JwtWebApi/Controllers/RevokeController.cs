5c4ed66cf8fee4a880bf7335b3d8f5a7c3e10cc6
ekapituro <ekapituro@gmail.com>
2025-02-10 18:29:27 +0100

    Update

---
+++ Start/DotNet.JwtWebApi/Controllers/RevokeController.cs
@@ -0,0 +1,37 @@
+﻿using DotNet.JwtWebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.AspNetCore.Http.StatusCodes;
+
+namespace DotNet.JwtWebApi.Controllers;
+
+[Route("api/account")]
+[ApiController]
+public class RevokeController(UserManager<ApplicationUser> userManager, ILogger<RevokeController> logger) : ControllerBase
+{
+    [Authorize]
+    [HttpDelete("Revoke")]
+    [ProducesResponseType(Status200OK)]
+    [ProducesResponseType(Status401Unauthorized)]
+    public async Task<IActionResult> Revoke()
+    {
+        try
+        {
+            var username = HttpContext.User.Identity?.Name;
+            if (username is null) return Unauthorized();
+
+            var user = await userManager.FindByNameAsync(username);
+            if (user is null) return Unauthorized();
+ 
+            user.RefreshToken = null;
+            await userManager.UpdateAsync(user);
+            return Ok();
+        }
+        catch (Exception exception)
+        {
+            logger.LogError(exception, nameof(Revoke));
+            return Ok();
+        }
+    }
+}
